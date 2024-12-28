35d6b3e07cfb7d130d13a885295c1ee573c1b3dc
Hikaru <105549697+my12parsecs@users.noreply.github.com>
2024-12-28 10:29:05 +0900

    sort test

--- src/app/trips/[slug]/page.js
+++ src/app/trips/[slug]/page.js
@@ -3,7 +3,7 @@ import { MinimalViable } from "@/app/components/SortableTree"
 export default function EachTrip() {
 
     return (
-        <div className="each-trip-page" style={{paddingTop: "50px"}}>
+        <div className="each-trip-page" style={{paddingTop: "50px", height: "100dvh", overflowY: "none"}}>
             <MinimalViable />
         </div>
     )
