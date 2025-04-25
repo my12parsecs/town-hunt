f43bdf691194ffac28a811f92588d8536f5f1f67
Hikaru <105549697+my12parsecs@users.noreply.github.com>
2025-04-26 00:48:41 +0900

    test

--- src/app/trips/page.js
+++ src/app/trips/page.js
@@ -1,134 +1,124 @@
-"use client";
+// "use client";
 
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleLeft, faTrash } from "@fortawesome/free-solid-svg-icons";
-import { getUserLocale } from "get-user-locale";
-import dayjs from "dayjs";
+// import { useEffect, useState } from "react";
+// import Link from "next/link";
+// import { useRouter } from "next/navigation";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faAngleLeft, faTrash } from "@fortawesome/free-solid-svg-icons";
+// import { getUserLocale } from "get-user-locale";
+// import dayjs from "dayjs";
 
-import getFlagEmoji from "@/app/components/GetFlagEmoji";
-import "../stylesheets/trips.css";
+// import getFlagEmoji from "@/app/components/GetFlagEmoji";
+// import "../stylesheets/trips.css";
 
-const Trips = () => {
+// const Trips = () => {
 
-  const [isInitialized, setIsInitialized] = useState(false);
-  const [isEditing, setIsEditing] = useState(false);
+//   const [isInitialized, setIsInitialized] = useState(false);
+//   const [isEditing, setIsEditing] = useState(false);
 
-  const [trips, setTrips] = useState([]);
-  const router = useRouter();
+//   const [trips, setTrips] = useState([]);
+//   const router = useRouter();
 
-  useEffect(() => {
-    // Load all trips from localStorage
-    const loadTrips = () => {
-      const allTrips = [];
-      for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        if (key.startsWith("town-hunt-trip-")) {
-          try {
-            const tripData = JSON.parse(localStorage.getItem(key));
-            allTrips.push({ ...tripData, id: key });
-          } catch (error) {
-            console.error("Error parsing trip data:", error);
-            // Handle corrupted data by removing it
-            localStorage.removeItem(key);
-          }
-        }
-      }
-      setTrips(allTrips);
-      setIsInitialized(true);
-    };
+//   useEffect(() => {
+//     // Load all trips from localStorage
+//     const loadTrips = () => {
+//       const allTrips = [];
+//       for (let i = 0; i < localStorage.length; i++) {
+//         const key = localStorage.key(i);
+//         if (key.startsWith("town-hunt-trip-")) {
+//           try {
+//             const tripData = JSON.parse(localStorage.getItem(key));
+//             allTrips.push({ ...tripData, id: key });
+//           } catch (error) {
+//             console.error("Error parsing trip data:", error);
+//             // Handle corrupted data by removing it
+//             localStorage.removeItem(key);
+//           }
+//         }
+//       }
+//       setTrips(allTrips);
+//       setIsInitialized(true);
+//     };
 
-    loadTrips();
-
-
-    const userLocale = getUserLocale();
-    let langy = userLocale.slice(0, 2);
-    // langy = "en"
-    require(`dayjs/locale/${langy}`);
-    dayjs.locale(langy);
-
-    let localizedFormat = require(`dayjs/plugin/localizedFormat`);
-    dayjs.extend(localizedFormat);
-  }, []);
+//     loadTrips();
 
 
-  const addNewTrip = () => {
-    const newTripId = `${Date.now()}${Math.random().toString(36).substr(2, 5)}`;
-    const newTripKey = `town-hunt-trip-${newTripId}`;
-    const newTrip = {
-      title: "Enter Trip Title",
-      id: newTripId,
-      startDate: "",
-      numOfDays: 0,
-      uniqueCountries: [],
-      trip: [],
-    };
-    localStorage.setItem(newTripKey, JSON.stringify(newTrip));
-    router.push(`/trips/${newTripKey}`);
-  };
+//     const userLocale = getUserLocale();
+//     let langy = userLocale.slice(0, 2);
+//     // langy = "en"
+//     require(`dayjs/locale/${langy}`);
+//     dayjs.locale(langy);
 
-  const deleteTrip = (tripId) => {
-    localStorage.removeItem(tripId);
-    setTrips((prevTrips) => prevTrips.filter((trip) => trip.id !== tripId));
-  };
+//     let localizedFormat = require(`dayjs/plugin/localizedFormat`);
+//     dayjs.extend(localizedFormat);
+//   }, []);
 
-  return (
-    <div className="trips-page">
-      <div className="trips-nav" style={{marginTop: "20px", marginBottom: "20px"}}>
-          <Link href="/">
-            <FontAwesomeIcon icon={faAngleLeft} style={{width: "20px", height: "20px"}} />
-          </Link>
-      </div>
 
-      <div className="trips-header">
-        <h1>Trips</h1>
-        <div style={{display: "flex", alignItems: "center"}}>
-          <div className="trips-edit-button" onClick={() => setIsEditing(!isEditing)}>{isEditing ? "Done" : "Edit"}</div>
-          <div className="trips-new-trip-button" onClick={addNewTrip}>New Trip</div>
-        </div>
-      </div>
+//   const addNewTrip = () => {
+//     const newTripId = `${Date.now()}${Math.random().toString(36).substr(2, 5)}`;
+//     const newTripKey = `town-hunt-trip-${newTripId}`;
+//     const newTrip = {
+//       title: "Enter Trip Title",
+//       id: newTripId,
+//       startDate: "",
+//       numOfDays: 0,
+//       uniqueCountries: [],
+//       trip: [],
+//     };
+//     localStorage.setItem(newTripKey, JSON.stringify(newTrip));
+//     router.push(`/trips/${newTripKey}`);
+//   };
 
-      <div className="trips-list">
+//   const deleteTrip = (tripId) => {
+//     localStorage.removeItem(tripId);
+//     setTrips((prevTrips) => prevTrips.filter((trip) => trip.id !== tripId));
+//   };
+
+//   return (
+//     <div className="trips-page">
+//       <div className="trips-nav" style={{marginTop: "20px", marginBottom: "20px"}}>
+//           <Link href="/">
+//             <FontAwesomeIcon icon={faAngleLeft} style={{width: "20px", height: "20px"}} />
+//           </Link>
+//       </div>
+
+//       <div className="trips-header">
+//         <h1>Trips</h1>
+//         <div style={{display: "flex", alignItems: "center"}}>
+//           <div className="trips-edit-button" onClick={() => setIsEditing(!isEditing)}>{isEditing ? "Done" : "Edit"}</div>
+//           <div className="trips-new-trip-button" onClick={addNewTrip}>New Trip</div>
+//         </div>
+//       </div>
+
+//       <div className="trips-list">
         
-          {trips.map((trip) => (
-            <div className="trips-each-container" key={trip.id}>
-              <Link href={`/trips/${trip.id}`} className="trips-each">
-                <div className="trips-title">{trip.title}</div>
-                <div className="trips-date-row">
-                  {trip.numOfDays !== 0 && <div className="trips-date">{trip.numOfDays} {trip.numOfDays === 1 ? "Day" : "Days" }</div>}
-                  {trip.startDate && <div className="trips-date">&nbsp;- {dayjs(trip.startDate).format("l")} ~{" "}{dayjs(trip.startDate).add(trip.numOfDays - 1, "day").format("l")}</div>}
-                </div>
-                {trip.uniqueCountries && <div className="trips-country">{trip.uniqueCountries.map((countryCode) => getFlagEmoji(countryCode)).join(" ")}</div>}
-              </Link>
-              {isEditing && (
-                  <div className="trips-delete-button" onClick={() => deleteTrip(trip.id)}>
-                    <FontAwesomeIcon icon={faTrash} style={{width: "15px", height: "15px"}} />
-                  </div>
-              )}
-            </div>
-          ))}
+//           {trips.map((trip) => (
+//             <div className="trips-each-container" key={trip.id}>
+//               <Link href={`/trips/${trip.id}`} className="trips-each">
+//                 <div className="trips-title">{trip.title}</div>
+//                 <div className="trips-date-row">
+//                   {trip.numOfDays !== 0 && <div className="trips-date">{trip.numOfDays} {trip.numOfDays === 1 ? "Day" : "Days" }</div>}
+//                   {trip.startDate && <div className="trips-date">&nbsp;- {dayjs(trip.startDate).format("l")} ~{" "}{dayjs(trip.startDate).add(trip.numOfDays - 1, "day").format("l")}</div>}
+//                 </div>
+//                 {trip.uniqueCountries && <div className="trips-country">{trip.uniqueCountries.map((countryCode) => getFlagEmoji(countryCode)).join(" ")}</div>}
+//               </Link>
+//               {isEditing && (
+//                   <div className="trips-delete-button" onClick={() => deleteTrip(trip.id)}>
+//                     <FontAwesomeIcon icon={faTrash} style={{width: "15px", height: "15px"}} />
+//                   </div>
+//               )}
+//             </div>
+//           ))}
 
-        {/* US Road Trip example - you can remove this if not needed */}
-        {/* <Link href="/trips/us-road-trip" className="trips-each">
-          <div className="trips-title">US Road Trip</div>
-          <div className="trips-date">Nov13~30</div>
-          <div className="trips-country">United States, Canada</div>
-        </Link> */}
-      </div>
-      {isInitialized && trips.length === 0 && (
-          <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100px"}}>
-            <div>No Trips</div>
-          </div>
-      )}
-      {/* {trips.length === 0 && (
-          <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100px"}}>
-            <div>No Trips</div>
-          </div>
-      )} */}
-    </div>
-  );
-};
 
-export default Trips;
+//       </div>
+//       {isInitialized && trips.length === 0 && (
+//           <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100px"}}>
+//             <div>No Trips</div>
+//           </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Trips;
