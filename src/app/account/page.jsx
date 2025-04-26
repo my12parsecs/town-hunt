795ad768e081b65866fe48715e351f300739d969
Hikaru <105549697+my12parsecs@users.noreply.github.com>
2025-04-26 22:09:51 +0900

    account test delete, favicon

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -1,11 +1,32 @@
 
-"use client"
 
 import Link from "next/link";
-import { useSession } from "../components/Session"
+import { cookies } from 'next/headers'
 
-export default function Account() {
-    const { session, isLoading, error, logout } = useSession()
+const BACKEND_DOMAIN = process.env.NEXT_PUBLIC_BACKEND_DOMAIN
+
+export default async function Account() {
+    const cookieStore = await cookies()
+    const token = cookieStore.get('session_token')?.value
+
+    let sessionData = []
+
+    try {
+    const session = await fetch(`${BACKEND_DOMAIN}/session`, {
+        method: 'GET',
+        headers: {
+        'Authorization': `Bearer ${token}`,
+        },
+        credentials: 'include',
+    })
+    if (session.ok) {
+        const json = await session.json()
+        // sessionData = Array.isArray(json) ? json : []
+        sessionData = json
+    }
+    } catch (error) {
+    console.error('Fetch error:', error)
+    }
     
   return (
     <div className="account-page" style={{width: "100%", maxWidth: "500px", margin: "auto"}}>
@@ -13,11 +34,11 @@ export default function Account() {
         <Link href="/">Back to Home</Link>
         <br />
         <br />
-        {session?.googleId ? (
+        {sessionData?.googleId ? (
             <div>
-                <img src={session?.image} style={{width: "100px", height: "100px", borderRadius: "50%", backgroundColor: "gray"}}></img>
-                <p>{session?.email}</p>
-                <p>{session?.name}</p>
+                <img src={sessionData?.image} style={{width: "100px", height: "100px", borderRadius: "50%", backgroundColor: "gray"}}></img>
+                <p>{sessionData?.email}</p>
+                <p>{sessionData?.name}</p>
             </div>
         ) : (
         <div>
